0be660858eb5f29d996d97b215f7bd0e8c6bcc4b
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2021-12-16 17:14:10 -0300

    OrderStatus enum

---
+++ src/main/java/com/vinolemos/course/entities/enums/OrderStatus.java
@@ -0,0 +1,31 @@
+package com.vinolemos.course.entities.enums;
+
+public enum OrderStatus {
+
+	// Enums recebem valor numérico fixo
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	// Converte um valor numérico para um tipo enumerado
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+}
