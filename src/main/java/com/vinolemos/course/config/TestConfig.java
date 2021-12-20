5f5ddacbccb307f207525cc0a22408a658775030
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2021-12-20 16:40:51 -0300

    Payment one-to-one association

--- src/main/java/com/vinolemos/course/config/TestConfig.java
+++ src/main/java/com/vinolemos/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.vinolemos.course.entities.Category;
 import com.vinolemos.course.entities.Order;
 import com.vinolemos.course.entities.OrderItem;
+import com.vinolemos.course.entities.Payment;
 import com.vinolemos.course.entities.Product;
 import com.vinolemos.course.entities.User;
 import com.vinolemos.course.entities.enums.OrderStatus;
@@ -72,7 +73,6 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-21T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
-		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
@@ -82,5 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
